18257664194e8da4b243682dcc890b3f7007fb44
Docente UC <docente@continental.edu.pe>
2026-01-12 19:20:54 -0500

    Se formateo el c+odigo con autopep8

--- biblioteca.py
+++ biblioteca.py
@@ -1,28 +1,29 @@
 class Biblioteca:
- def __init__(self,name):
-  self.name = name
-  self.libros=[]
+    def __init__(self, name):
+        self.name = name
+        self.libros = []
 
- def addb(self,book):
-  self.libros.append(book)
+    def addb(self, book):
+        self.libros.append(book)
 
- def show(self):
-  for libro in self.libros:
-   print(libro.titulo,libro.autor,libro.indice)
+    def show(self):
+        for libro in self.libros:
+            print(libro.titulo, libro.autor, libro.indice)
+
 
 class Book:
- def __init__(self,titulo,autor,indice):
-  self.titulo = titulo
-  self.autor = autor
-  self.indice = indice
-  self.exist = True
+    def __init__(self, titulo, autor, indice):
+        self.titulo = titulo
+        self.autor = autor
+        self.indice = indice
+        self.exist = True
 
- def lend(self):
-  if self.exist == True:
-   self.exist = False
-   return True
-  else:
-   return False
+    def lend(self):
+        if self.exist == True:
+            self.exist = False
+            return True
+        else:
+            return False
 
- def back(self):
-  self.exist=True
+    def back(self):
+        self.exist = True
